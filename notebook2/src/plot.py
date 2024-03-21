9bd3d0bd3d372c9e2f716a41b0d913d2a663e108
lschneid <lisa.schneider@epfl.ch>
2024-03-21 17:10:46 +0100

    feat(notebook2): Solution for third exercises updated

--- notebook2/src/plot.py
+++ notebook2/src/plot.py
@@ -157,7 +157,7 @@ def plot_log_lines(list_to_plot, list_name, axis_label, folder, title, x_axis = 
         plt.savefig('../plot/log'+title+'.pdf')
 
 def plot_lines(list_to_plot, list_name, axis_label, folder, title, x_axis = None, show = False):
-    plt.style.use('seaborn')
+    # plt.style.use('seaborn')
     n_iter = list_to_plot[0].size
     plt.figure()
     for _, item in enumerate(zip(list_to_plot, list_name)):
